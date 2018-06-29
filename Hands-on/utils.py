fed3763fd48fed5989361d8e9017a0d83b10ca56
Piotr Januszewski <piojanuszewski@gmail.com>
2018-06-29 11:13:37 +0200

    Generate rental car problem dynamics

--- Hands-on/utils.py
+++ Hands-on/utils.py
@@ -1,61 +1,106 @@
+from collections import defaultdict
+from itertools import product
+from scipy import stats
+
 import numpy as np
 
-lake_annot = [
-    np.array([
-        ['S', 'F', 'F', 'F'],
-        ['F', 'H', 'F', 'H'],
-        ['F', 'F', 'F', 'H'],
-        ['H', 'F', 'F', 'G']]),
-    np.array([
-        ['S', 'F', 'F', 'F', 'F', 'F', 'F', 'F'],
-        ['F', 'F', 'F', 'F', 'F', 'F', 'F', 'F'],
-        ['F', 'F', 'F', 'H', 'F', 'F', 'F', 'F'],
-        ['F', 'F', 'F', 'F', 'F', 'H', 'F', 'F'],
-        ['F', 'F', 'F', 'H', 'F', 'F', 'F', 'F'],
-        ['F', 'H', 'H', 'F', 'F', 'F', 'H', 'F'],
-        ['F', 'H', 'F', 'F', 'H', 'F', 'H', 'F'],
-        ['F', 'F', 'F', 'H', 'F', 'F', 'F', 'G']])
-]
 
+def poisson(k, k_max, lam):
+    """Truncated Poisson distribution"""
 
-def action2str(a):
-    if a == 0:
-        return 'L'
-    elif a == 1:
-        return 'D'
-    elif a == 2:
-        return 'R'
-    elif a == 3:
-        return 'U'
+    if (k, k_max, lam) in poisson.cache:
+        return poisson.cache[(k, k_max, lam)]
+
+    if k < k_max:
+        p = stats.poisson.pmf(k, lam)
+    elif k == k_max:
+        # basically it's mass of the right tail
+        p = 1 - stats.poisson.cdf(k_max - 1, lam)
     else:
-        return '-'
+        assert False, 'k > k_max'
+
+    poisson.cache[(k, k_max, lam)] = p
+    return p
 
 
-def vi_frozen_lake(env):
-    # MDP transition/dynamics
-    P = env.env.P
-    n_states, n_actions = env.observation_space.n, env.action_space.n
+poisson.cache = {}
 
-    V = np.zeros(n_states)
 
-    while True:
-        delta = 0
-        # Start from the end to speed value propagation
-        for s in reversed(range(n_states)):
-            oldV = V[s]
-            V[s] = np.max([sum([p*(r + V[s_]) for p, s_, r, _ in P[s][a]])
-                           for a in range(n_actions)])
-            delta = max(delta, abs(oldV - V[s]))
+def get_dynamics(rent_reward=10,
+                 expected_req1=3,
+                 expected_req2=4,
+                 expected_ret1=3,
+                 expected_ret2=2,
+                 max_parking=20,
+                 max_transfer=5):
 
-        if delta < 1e-30:
-            break
+    T = np.zeros([max_parking + 1, max_parking + 1, max_transfer *
+                  2 + 1, max_parking + 1, max_parking + 1, 2])
 
-    pi_str = []
-    for s in range(n_states):
-        pi = np.argmax([sum([p*(r + V[s_]) for p, s_, r, _ in P[s][a]]) for a in range(n_actions)])
-        pi_str.append(action2str(pi if len(P[s][0]) > 1 else 5))
+    def _actions_in_state(n1, n2):
+        """Iterator over all actions in state"""
+        assert n1 <= max_parking
+        assert n2 <= max_parking
 
-    # Overide value of goal state to make graph nicer
-    V[-1] = 1
+        for a in range(min(n1, max_transfer) + 1):
+            if (a + n2) > max_parking:
+                break
+            yield a
 
-    return V, pi_str
+        # since a=0 was already yielded, start with 1
+        for a in range(1, min(n2, max_transfer) + 1):
+            if (a + n1) > max_parking:
+                break
+            yield -a
+
+    def _simulation(n_cars, expected_req, expected_ret):
+        """Iterator over all possible transitions from given number of cars"""
+        p_total = 0.0
+        for req in range(n_cars + 1):
+            p_req = poisson(req, n_cars, expected_req)
+            max_ret = max_parking - (n_cars - req)
+            for ret in range(max_ret + 1):
+                p_ret = poisson(ret, max_ret, expected_ret)
+                yield req, p_req, ret, p_ret
+                p_total += p_req * p_ret
+        assert np.allclose(p_total, 1.), 'Total simulation probability must be 1'
+
+    # For all the states
+    for n1, n2 in product(range(max_parking + 1), repeat=2):
+        # For all actions
+        for a in _actions_in_state(n1, n2):
+            # Perform action
+            n1_post = n1 - a
+            n2_post = n2 + a
+            p_total = 0
+
+            # Simulate day in car rentals, iterate over all possibilities
+            for req1, p_req1, ret1, p_ret1 in _simulation(n1_post,
+                                                          expected_req1,
+                                                          expected_ret1):
+                for req2, p_req2, ret2, p_ret2 in _simulation(n2_post,
+                                                              expected_req2,
+                                                              expected_ret2):
+                    # Calculate next states
+                    n1_next = n1_post - req1 + ret1
+                    n2_next = n2_post - req2 + ret2
+
+                    # Checks...
+                    assert n1_next >= 0, 'n1_next >= 0'
+                    assert n2_next >= 0, 'n2_next >= 0'
+                    assert n1_next <= max_parking, 'n1_next <= self.max_parking'
+                    assert n2_next <= max_parking, 'n2_next <= self.max_parking'
+
+                    # Next state probability and reward
+                    p = p_req1 * p_ret1 * p_req2 * p_ret2
+                    r = rent_reward * (req1 + req2)
+
+                    # Update dynamics
+                    T[n1, n2, a, n1_next, n2_next] += p, p * r
+
+                    # Book keeping
+                    p_total += p
+            assert np.allclose(p_total, 1.), 'Total transition probability must be 1'
+        print("Set T[({}, {}), ...]".format(n1, n2))
+
+    return T
